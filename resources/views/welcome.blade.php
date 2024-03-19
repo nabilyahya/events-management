159c3712a7b1e585f3c301ba686fe7d74c90acce
nabilyahya <nabilhero12@gmail.com>
2024-03-19 18:01:01 +0300

    some edits for event

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,12 +37,19 @@
                                         Events
                                     </a>
                                 @else
+                                 <a
+                                    href="events/weekly"
+                                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                                >
+                                Events
+                                </a>
                                     <a
                                         href="{{ route('login') }}"
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
                                     >
                                         Log in
                                     </a>
+                                 
 
                                     @if (Route::has('register'))
                                         <a
